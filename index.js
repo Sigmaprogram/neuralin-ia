63be9ac716d0707f68988198251a6e73678b199c
Kervin Leonardo <sdeclases48@gmail.com>
2024-10-18 20:56:06 -0400

    logica casi terminada, faltan las transiciones y el scroll automatico por mencionar algunas cosas

--- index.js
+++ index.js
@@ -17,26 +17,47 @@ body.addEventListener("click", (event) => {
 // Api request
 const apiKey = "AIzaSyCEhUUxSViWyF0j4RiCAEvuAd51UzGde4k";
 const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+const responseContainer = document.getElementById("response-area");
 
 document
   .getElementById("send-btn")
   .addEventListener("click", async function () {
     const userInputElement = document.getElementById("user-input");
     if (userInputElement.value !== "") {
+      // ocultar clean-response-container
+      const cleanResponseContainer = document.querySelector(
+        ".clean-response-container"
+      );
+      cleanResponseContainer.style.display = "none";
       const input = userInputElement.value;
       userInputElement.value = ""; // Limpiar el campo de texto
+      // Mostrar caja de pregunta
+      responseContainer.innerHTML += `
+      <div class="ask-box">
+        <div class="ask-text">
+          <p class="ask-text-p">
+            ${input}
+          </p>
+        </div>
+      </div>
+      <!-- Fin de caja de pregunta -->
+    `;
 
       // Esperar el resultado de la función async
       try {
         const respuesta = await generarRespuesta(input);
-        // ocultar clean-response-container
-        const cleanResponseContainer = document.querySelector(
-          ".clean-response-container"
-        );
-        cleanResponseContainer.style.display = "none";
-
         // mostrar response-area
-        const responseContainer = document.getElementById("response-area");
+        responseContainer.innerHTML += `
+          <!-- Caja de respuesta -->
+          <div class="response-box">
+            <h4 class="response-title">Neuralin</h4>
+            <div class="response-text">
+              <p class="response-text-p">${respuesta.candidates[0].content.parts[0].text}</p>
+            </div>
+          </div>
+          <!-- Fin de caja de respuesta -->
+        `;
+        responseArea.scrollTop = responseArea.scrollHeight;
       } catch (error) {
         console.error("Error al generar respuesta:", error);
       }
